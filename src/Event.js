8ce32b722de83b5c3dbc9fcfb571fc3353c8f63f
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-08-17 18:02:20 +0400

    Update Event.js

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
+
+
 class Event extends Component {
+  state = {
+    show: false,
+  };
+
+  handleButton = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+
   render() {
-    return <div></div>;
+    const event = this.props.event;
+    return (
+      <ul className="event">
+        <li className="status">{event.status}</li>
+        <li className="htmlLink">{event.htmlLink}</li>
+        <li className="summary">{event.summary}</li>
+        <li className="description">{event.description}</li>
+        <li className="location">{event.location}</li>
+        <li className="start">{event.start.dateTime}</li>
+        <li className="end">{event.end.dateTime}</li>
+
+        {this.state.show === false && (
+          <button className="showMore" onClick={() => this.handleButton()}>
+            Show more
+          </button>
+        )}
+        {this.state.show === true && (
+          <button className="showLess" onClick={() => this.handleButton()}>
+            Show less
+          </button>
+        )}
+      </ul>
+    );
   }
 }
 
-export default Event
+export default Event;
