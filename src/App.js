833865112326acab9069dc33c3733019eff19617
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-08-18 10:43:24 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,53 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
+import { getEvents } from './api';
+
 class App extends Component {
+  state = {
+    events: [],
+    numberOfEvents: 32,
+    infoText: '',
+    locations: []
+  }
+
+
+
+
+  updateEvents = (location, eventCount) => {
+    let locationEvents;
+    getEvents().then((events) => {
+      if (location === 'all' && eventCount === 0) {
+        locationEvents = events;
+      } else if (location !== 'all' && eventCount === 0) {
+        locationEvents = events.filter((event) => event.location === location);
+      } else if (location === '' && eventCount > 0) {
+        locationEvents = events.slice(0, eventCount);
+      } else if (location === '' && eventCount === '') {
+        locationEvents = events;
+      }
+      this.setState({
+        events: locationEvents,
+        numberOfEvents: eventCount,
+      });
+    });
+  };
+
+
+
+
+
   render() {
+
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
-        <NumberOfEvents />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+          numberOfEvents={this.state.numberOfEvents} />
+        <EventList events={this.state.events} />
+        <NumberOfEvents updateEvents={this.updateEvents}
+          numberOfEvents={this.state.numberOfEvents} />
       </div>
     );
   }
