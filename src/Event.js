42275d897596b035c594d92a1d87e77f7ac0db33
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-08-24 16:56:57 +0400

    Update Event.js

--- src/Event.js
+++ src/Event.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 
 class Event extends Component {
   state = {
-    showHideDetails: false,
+    show: false,
   };
 
   handleButton = () => {
@@ -15,14 +15,15 @@ class Event extends Component {
     const event = this.props.event;
     return (
       <ul className="event">
-        <li className="status">{event.status}</li>
-        <li className="htmlLink">{event.htmlLink}</li>
-        <li className="summary">{event.summary}</li>
-        <li className="location">{event.location}</li>
-        <li className="start">{event.start.dateTime}</li>
-        <li className="end">{event.end.dateTime}</li>
+        <h3>Event Information</h3>
+        <li className="status">Status: {event.status}</li>
+        <li className="htmlLink">Link: {event.htmlLink}</li>
+        <li className="summary">Summary: {event.summary}</li>
+        <li className="location">Location: {event.location}</li>
+        <li className="start">Event Start Date and Time:{event.start.dateTime}</li>
+        <li className="end">Event End Date and Time{event.end.dateTime}</li>
         {this.state.show === true && (
-          <p className="details">{event.description}</p>
+          <p className="details">Description:{event.description}</p>
         )}
         {this.state.show === false && (
           <button className="showMore" onClick={() => this.handleButton()}>
