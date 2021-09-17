0084b74118e4b003ff092b38d65f6fb59f53b4ee
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-09-17 10:33:48 +0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -84,7 +84,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = "https://dff5rv0kx0.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/" + token;
+    const url = `https://dff5rv0kx0.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${token}`;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
