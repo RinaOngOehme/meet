be6563fff441bbea9146b14f7391884cf6d4751e
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-09-22 22:55:42 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Meet App </h1>
-        <h4>Choose your nearest city</h4>
+
         <CitySearch
           updateEvents={this.updateEvents}
           locations={locations} />
